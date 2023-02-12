c2ecd721a34923fc28c7cf68663f6691edea9ac1
Alan Pisani <alanpisani1993@gmail.com>
2023-02-12 13:02:48 -0300

    Testeando boton mas y boton menos con console.log

---
+++ main.js
@@ -0,0 +1,13 @@
+let minusBtn = document.querySelector('.input__minus');
+let userInput = document.querySelector('.input__number');
+let plusBtn = document.querySelector('.input__plus');
+
+let userInputNumber = 0;
+
+plusBtn.addEventListener('click', ()=>{
+    console.log("se presiono mas");
+});
+
+minusBtn.addEventListener('click', ()=>{
+    console.log("se presiono menos");
+});
