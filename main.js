b04c5f8b4585b60ae73380a9838bfcf246ba5f72
Alan Pisani <alanpisani1993@gmail.com>
2023-02-12 16:42:28 -0300

    Terminado funcion del boton añadir al carrito. Activadas notificaciones de carrito

--- main.js
+++ main.js
@@ -1,19 +1,38 @@
-let minusBtn = document.querySelector('.input__minus');
-let userInput = document.querySelector('.input__number');
-let plusBtn = document.querySelector('.input__plus');
+//cambio de cantidad de articulos ingresado por el usuario
+
+let minusBtn = document.querySelector(".input__minus");
+let userInput = document.querySelector(".input__number");
+let plusBtn = document.querySelector(".input__plus");
 
 let userInputNumber = 0;
 
-plusBtn.addEventListener('click', ()=>{
-    userInputNumber++;
-    console.log(userInputNumber);
+plusBtn.addEventListener("click", () => {
+  userInputNumber++;
+  userInput.value = userInputNumber;
+  console.log(userInputNumber);
 });
 
-minusBtn.addEventListener('click', ()=>{
-    userInputNumber--;
-    if (userInputNumber <= 0){
-        
-        userInputNumber = 0;
-    }
-    console.log(userInputNumber);
+minusBtn.addEventListener("click", () => {
+  userInputNumber--;
+  if (userInputNumber <= 0) {
+    userInputNumber = 0;
+  }
+  userInput.value = userInputNumber;
+  console.log(userInputNumber);
 });
+
+//Agregar el total de productos al carrito cuando se presiona el boton AÑADIR AL CARRITO
+
+const addToCartBtn = document.querySelector('.details__button');
+let cartNotification = document.querySelector('.header__cart--notification');
+
+addToCartBtn.addEventListener('click', ()=>{
+
+    let lastValue = parseInt(cartNotification.innerText);
+    lastValue = lastValue + userInputNumber;
+
+    cartNotification.innerText = lastValue;
+    cartNotification.style.display = "block";
+    
+});
+
