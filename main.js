9700fd68baedd3ca8a5dfcc828ffc4fabd8eae35
Alan Pisani <alanpisani1993@gmail.com>
2023-02-17 14:34:54 -0300

    Creando pagina index

--- main.js
+++ main.js
@@ -153,24 +153,6 @@ nextModalBtn.addEventListener('click', ()=>{
 });
 
 
-//Mostrar el navbar version celular clickeando la hamburguesa
-
-const hamburgerMenu = document.querySelector('.header__menu');
-const modalNavbar = document.querySelector('.modal-navbar__background');
-const closeModalNavbar = document.querySelector('.modal-navbar__close-icon');
-
-//modalNavbar.style.display = 'none';
-
-hamburgerMenu.addEventListener('click', ()=>{
-  console.log('abrir modal');
-  modalNavbar.style.display = 'block';
-});
-
-closeModalNavbar.addEventListener('click', ()=>{
-  modalNavbar.style.display = 'none';
-});
-
-
 //FUNCIONES
 
 function drawProductInModal() {
